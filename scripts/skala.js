67248542f3013fc075f84bdcf93937dd7eda0efb
vinzenzzinecker98 <vinzenz@zinecker.de>
2022-04-12 13:52:49 +0200

    first commit

---
+++ scripts/skala.js
@@ -0,0 +1,47 @@
+function computeskala(){
+    var number = document.getElementById('skalanumber').value;
+    var passwort = document.getElementById("salt").value;
+    var code = encrypt(number, passwort);    
+    document.getElementById("textfield").value = code ;
+  }
+  function decryptskala(){
+      var code = document.getElementById("textfield-decrypt");
+      var password = document.getElementById("pw-decrypt");
+      var decrypted = decrypt(code, password);
+      document.getElementById("zahl-decrypt").value=decrypted;
+  }
+
+  function encrypt (number, passwort){
+      var pw = CryptoJS.SHA256(passwort);
+      var encrypted = CryptoJS.AES.encrypt(number, String(pw));
+      h = CryptoJS.SHA256(pw + encrypted.salt); // Hash von Passwort-Hash + Salt
+      var e = h + encrypted.salt + String(encrypted); //alert(e.length); // encrypt ist vom type object
+      return e
+  }
+
+  function decrypt(code, passwort){
+      code=code.value;
+      passwort=passwort.value;
+      var pws = code.substring(0, 80); //alert (pws); 	// 80 zeichen hash+salt
+      var pw_h2 = pws.substring(0, 64); //alert("hash: "+pw_h2); 		// Übermittelter Hash (aus Passwort + Salt)
+      var salt = pws.substr(64); //alert (salt); 		// Salt 16 Zeichen	
+          
+      var pw = CryptoJS.SHA256(passwort); // Hash Passwort 
+      
+      pw_h1 = CryptoJS.SHA256(pw + salt); // Hash aus Passwort-Hash + salt
+
+          // Hashs vergleichen
+          if (pw_h1 != pw_h2) {                              
+              alert ("Das Passwort ist falsch!"); //immer schön Alörts setzen
+              return ""
+          } else {
+              // Wenn die Passworte übereinstimmen                
+              encrypted = code.substr(80); // Passworthash + salt(80) abschneiden
+
+              var decrypted = CryptoJS.AES.decrypt(encrypted, String(pw)) // entschlüsseln
+
+              decrypted = decrypted.toString(CryptoJS.enc.Utf8);
+              alert("Die Zahl ist: " + decrypted)
+              return decrypted;                
+          }            
+  }
