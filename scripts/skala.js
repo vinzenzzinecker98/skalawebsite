53abdebd5e6da8819e9657a35d6d89b1e319667c
vinzenzzinecker98 <vinzenz@zinecker.de>
2022-04-13 23:11:43 +0200

    scale textarea

--- scripts/skala.js
+++ scripts/skala.js
@@ -1,3 +1,12 @@
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].addEventListener("input", OnInput, false);
+}
+function OnInput() {
+  this.style.height = "auto";
+  this.style.height = (this.scrollHeight) + "px";
+}
 function computeskala(){
     var number = document.getElementById('skalanumber').value;
     var passwort = document.getElementById("salt").value;
